e4168ba2d9fc857e5a3a271c9b3e0bb317971ff7
OBo1307 <och.bogdan@gmail.com>
2023-12-18 09:37:25 +0000

    Exercise2.6
    ●	Create authentication for your web application
    ●	Use GET and POST methods
    ●	Password protect your web application’s views

--- src/recipe_project/urls.py
+++ src/recipe_project/urls.py
@@ -18,11 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf  import settings #added for media files
 from django.conf.urls.static import static #added for media files
+from .views import login_view, logout_view #added for authentication
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include ('recipes.urls')),
     path('recipes/', include ('recipes.urls')),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView # to display list of objects and details of a single object
-from .models import Recipe # to access the Recipe model
+from django.views.generic import ListView, DetailView     # to display list of objects and details of a single object
+from django.contrib.auth.mixins import LoginRequiredMixin # to restrict access to views
+from .models import Recipe                                # to access the Recipe model
 
 # Create your views here.
-class RecipeListView(ListView):
+class RecipeListView(LoginRequiredMixin, ListView):       # class-based 'protected' view
     model = Recipe
     template_name = 'recipes/main.html'
 
-class RecipeDetailView(DetailView):
+class RecipeDetailView(LoginRequiredMixin, DetailView):   # class-based 'protected' view
     model = Recipe
     template_name = 'recipes/detail.html'
 
